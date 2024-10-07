a22a35c9dd1f19fbcba771abbf4c176c31b84045
Gabriel Guarini <gabrielrguarini@gmail.com>
2024-10-07 20:45:44 -0300

    feat: ajust `auth.ts`

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,5 +1,17 @@
 import { RequestHandler } from "express";
+import { singupSchema } from "../schemas/singup";
 
 export const singup: RequestHandler = async (req, res) => {
-  res.json({});
+  try {
+    const safeData = singupSchema.safeParse(req.body);
+    if (!safeData.success) {
+      res.status(400).json({ error: safeData.error.flatten().fieldErrors });
+      return;
+    }
+
+    res.status(200).json({});
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
